841ccadea2d52c6bcef24dcb1437e1a3834f949d
AamHermansyah <amzhermanzyah@gmail.com>
2023-07-30 19:32:04 +0700

    [UPDATE] Link in hero section

--- components/pages/home/Hero.tsx
+++ components/pages/home/Hero.tsx
@@ -8,7 +8,7 @@ import { containerShow, itemShow } from '@/lib/animate'
 
 function Hero() {
   return (
-    <section id="hero" className="relative overflow-hidden">
+    <section id="home" className="relative overflow-hidden">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -43,34 +43,34 @@ function Hero() {
             <motion.h4 variants={itemShow(.5, false)} className="md:flex items-center gap-4 text-xl sm:text-4xl font-extrabold uppercase mt-2">
               We Can
               <span className="text-colorfull-red">
-              <Typewriter
-                options={{ loop: true }}
-                onInit={typewriter => {
-                  typewriter
-                    .pauseFor(2000)
-                    .typeString('WEB DESIGN')
-                    .pauseFor(1000)
-                    .deleteAll()
-                    .pauseFor(2000)
-                    .typeString('APP DEVELOPMENT')
-                    .pauseFor(1000)
-                    .deleteAll()
-                    .typeString('VIDEO EDITING')
-                    .pauseFor(1000)
-                    .deleteAll()
-                    .typeString('SOC-MED MANAGEMENT')
-                    .pauseFor(1000)
-                    .deleteAll()
-                    .typeString('DIGITAL MARKETING')
-                    .pauseFor(1000)
-                    .deleteAll()
-                    .typeString('SEO MANAGEMENT')
-                    .pauseFor(1000)
-                    .deleteAll()
-                    .typeString('GRAPHIC DESIGN')
-                    .pauseFor(1000)
-                    .start()
-                }} />
+                <Typewriter
+                  options={{ loop: true }}
+                  onInit={typewriter => {
+                    typewriter
+                      .pauseFor(2000)
+                      .typeString('WEB DESIGN')
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .pauseFor(2000)
+                      .typeString('APP DEVELOPMENT')
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString('VIDEO EDITING')
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString('SOC-MED MANAGEMENT')
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString('DIGITAL MARKETING')
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString('SEO MANAGEMENT')
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString('GRAPHIC DESIGN')
+                      .pauseFor(1000)
+                      .start()
+                  }} />
               </span>
             </motion.h4>
             <motion.p variants={itemShow(.5, false)} className="text-sm sm:text-base mt-4 pl-1 max-w-sm sm:max-w-2xl ">
